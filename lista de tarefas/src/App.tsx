1a0f1a5bcd0a37f2eca01ca1cc991cfbb561f7d7
Jean Oliveira <jeanoliveira2709@gmail.com>
2026-03-09 16:57:41 -0300

    implementando localStorage no Projeto.

--- lista de tarefas/src/App.tsx
+++ lista de tarefas/src/App.tsx
@@ -1,23 +1,50 @@
 import "./App.css"
 
 import { Button } from "./Button"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
+type Task = {
+  text: string
+  completed: boolean
+}
 
 export default function App () {
 
   const [input, setInput] = useState("") 
-  const [tasks, setTasks] = useState <string[]>([])
+  const [tasks, setTasks] = useState <Task[]>(() =>{
+    const savedTasks =  localStorage.getItem("tasks")
+    return savedTasks? JSON.parse(savedTasks) : []
+  })
+
+  useEffect (() => {
+     const savedTasks = localStorage.getItem("tasks")
+
+     if (savedTasks) {
+      setTasks(JSON.parse(savedTasks))
+     }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
 
   function handleAddTask() {
     if (input.trim() === "") return 
 
-    setTasks([...tasks, input])
+    setTasks([...tasks, {text: input, completed: false}])
     setInput("")
   }
 
   function handleDeleteTask(indexToDelete: number) {
     const newTasks = tasks.filter((_, index) => index !== indexToDelete)
     setTasks(newTasks)
+  }
+
+  function toggleTask(index: number) {
+    const updatedTasks = tasks.map((task, i) => 
+    i === index? {...task, completed: !task.completed} : task
+  )
+  setTasks(updatedTasks)
   }
 
 
@@ -45,11 +72,17 @@ export default function App () {
       <div className="tasks">
           {tasks.map((task, index) => (
             <div key={index} className="task">
-              <span>{task}</span>
+              <span className= {task.completed? "completed" : ""}>{task.text}</span>
             
             
-              <button className="delete" onClick={() => handleDeleteTask(index)}>🗑️</button>   
-              <input className="check" type="checkbox" />
+              <button className="delete" onClick={() => handleDeleteTask(index)}>🗑️</button>
+              
+              <input 
+                className="check" 
+                type="checkbox"
+                checked = {task.completed}
+                onChange={() => toggleTask(index)}
+              />
             </div>
           ))}
       </div>
